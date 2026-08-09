f68723dd9f337406e9a5b97c419b5f7b20e74b1c
Kyungmin Lim <lkm1216@gmail.com>
2026-08-09 15:24:01 +0900

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // Time complexity: O(n)
+        // Space complexity: O(1)
+        int num = prices.size();
+        int result = 0;
+        int buy = prices[0];
+
+        for(int i=1; i<num; i++)
+        {
+            buy = min(buy, prices[i]);
+            result = max(result, prices[i]-buy);
+        }
+        return result;
+        
+    }
+};
