7d0d532bc0fda63f575fe4d265f57278a24c1c28
Kyungmin Lim <lkm1216@gmail.com>
2026-08-09 15:05:07 +0900

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // Time complexity: O(n^2): n is the row/col of board
+        // Space complexity: O(n^2)
+        int row[9][9] = {0, 0};
+        int col[9][9] = {0, 0};
+        int blk[9][9] = {0, 0};
+
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                int blk_idx = (i/3)*3 + (j/3);
+                char val = board[i][j];
+                if(val == '.')
+                {
+                    continue;
+                }
+                int idx = val-'1';
+
+                if(row[i][idx]||col[j][idx]||blk[blk_idx][idx])
+                {
+                    return false;
+                }
+                row[i][idx]=1;
+                col[j][idx]=1;
+                blk[blk_idx][idx]=1;
+            }
+        }
+        return true;
+        
+    }
+};
