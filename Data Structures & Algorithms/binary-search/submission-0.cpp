305d517f289ef0efbfacbc8e5f5000d55c3aebcf
Kyungmin Lim <lkm1216@gmail.com>
2026-08-23 14:36:40 +0900

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int num = nums.size();
+        
+        int front = 0;
+        int end = num-1;
+        int idx = front + (end-front)/2;
+        int result = -1;
+
+        while(front <= end)
+        {
+            if(nums[idx]<target)
+            {
+                front = idx+1;                
+            }
+            else if(nums[idx]>target)
+            {
+                end = idx-1;
+            }
+            else
+            {
+                result = idx;
+                break;
+            }
+            idx = front + (end-front)/2;
+
+        }
+        return result;
+        
+    }
+};
