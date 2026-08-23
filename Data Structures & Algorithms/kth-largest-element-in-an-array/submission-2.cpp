93d1d83979a28990f73b9d53a9661aa97f2e73f3
Kyungmin Lim <lkm1216@gmail.com>
2026-08-23 17:14:59 +0900

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        /*
+        // Time complexity: O(NlogN)
+        // Space complexity: O(1)
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        
+        return nums[n-k];
+        */
+
+        // Time complexity: O(n)
+        // Space complexity: O(n)
+
+        priority_queue<int> maxHeap;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            maxHeap.push(nums[i]);
+        }
+
+        for(int i=0; i<k-1; i++)
+        {
+            maxHeap.pop();
+        }
+        return maxHeap.top();
+        
+    }
+};
